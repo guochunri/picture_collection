f62999c697f8916a2b3202d91884b0c87c8a4ebc
bater <jace_ka@yahoo.com.tw>
2017-11-07 23:37:00 +1100

    solve imagemagick issue

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# README
+# Start the server
 
+1. `brew install imagemagick` in your local machine.
+1. `brew upgrade imagemagick` to imagemagick 7.0.7-10, or you may fail at image resize. See [here](https://github.com/carrierwaveuploader/carrierwave/issues/1486#issuecomment-276548664).
 1. copy `application.example.yml` and rename as `applcation.yml`, paste your aws key and secret.
 1. bundle install
 1. rake db:migrate
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,12 +5,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # if Rails.env.development?
-  #   storage :file
-  # else
-  #   storage :fog
-  # end
-  storage :fog
+  if Rails.env.development?
+    storage :file
+  else
+    storage :fog
+  end
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -18,15 +18,15 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # process resize_to_fit: [800, 800]
+  process resize_to_fit: [800, 800]
 
-  # version :thumb do
-  #   process resize_to_fill: [200,200]
-  # end
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
 
-  # version :medium do
-  #   process resize_to_fill: [400,400]
-  # end
+  version :medium do
+    process resize_to_fill: [400,400]
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
