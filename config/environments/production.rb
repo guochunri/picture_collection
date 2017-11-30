9165aab11fae5c4d90f676ea518f7405725e4c3d
郭春日 <uochunri@gmail.com>
2017-11-30 11:00:58 +0800

    config s3 passsword

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_s3.default_url_options = { :host => '120.77.213.183' }
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
