48ae6ebe0da7b11b384a50f2a7aac744dcbc3121
郭春日 <uochunri@gmail.com>
2017-12-12 14:14:43 +0800

    Add final approve state option

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,7 +13,9 @@ class Product < ApplicationRecord
   aasm do
     state :waitting_for_approval, initial: true
     state :approved
+    state :final_approved
     state :rejected
+    state :final_rejected
 
     event :agree do
       transitions from: :waitting_for_approval,         to: :approved
@@ -23,8 +25,16 @@ class Product < ApplicationRecord
       transitions from: :waitting_for_approval,     to: :rejected
     end
 
+    event :final_agree do
+      transitions from: :approved,         to: :final_approved
+    end
+
+    event :final_disagree do
+      transitions from: :approved,         to: :final_rejected
+    end
+
     event :update_product do
-      transitions from: [:waitting_for_approval, :approved, :rejected], to: :waitting_for_approval
+      transitions from: [:waitting_for_approval, :approved, :rejected, :final_approved, :final_rejected], to: :waitting_for_approval
     end
   end
 
