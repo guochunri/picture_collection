a5a002a6472febc0dcce117ecb0e1ac9fb64a081
郭春日 <uochunri@gmail.com>
2017-12-13 15:57:24 +0800

    config another folder store images

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,3 +42,9 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+# created symlinlk: create another folder store images
+task :symlink_config, roles: :app do
+  
+  run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
+end
